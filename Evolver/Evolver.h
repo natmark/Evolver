bf34614275720373731abbefd2ff17ce4aea0b65
natmark <natmark0918@gmail.com>
2017-10-15 09:43:12 +0900

    :new: Add project

---
+++ Evolver/Evolver.h
@@ -0,0 +1,19 @@
+//
+//  Evolver.h
+//  Evolver
+//
+//  Created by AtsuyaSato on 2017/10/15.
+//  Copyright © 2017年 Atsuya Sato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Evolver.
+FOUNDATION_EXPORT double EvolverVersionNumber;
+
+//! Project version string for Evolver.
+FOUNDATION_EXPORT const unsigned char EvolverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Evolver/PublicHeader.h>
+
+

ce92981ede713dfabf4e80d1db24952eaf66c9a0
natmark <natmark0918@gmail.com>
2018-05-27 17:19:55 +0900

    :hammer: Refactor Evolver class

--- Evolver/Evolver.swift
+++ Evolver/Evolver.swift
@@ -15,48 +15,13 @@ public enum Result<T> {
 
 public class Evolver {
     public class func run<T: Generable>(template: T.Type, generations: Int, individuals: Int, completion: (_ model: T, _ generation: Int, _ individual: Int) -> Int) -> Result<T> {
-
-        // MARK: Check template
-        let geneTemplate = template.init()
-
-        let encoder = JSONEncoder()
-        let data = try! encoder.encode(geneTemplate)
-        guard let parameters = ((try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }) else {
+        guard let dictionary = try? Evolver.checkTemplate(template: template) else {
             return .failure(EvolverError.encodeError)
         }
 
-        var dictionary = [String: Array<[String : Int]>]()
-        for child in Mirror(reflecting: geneTemplate).children {
-            var array = [Dictionary<String, Int>]()
+        var genoms = Evolver.createInitialIndividuals(template, dictionary: dictionary, individuals: individuals)
 
-            guard
-                let label = child.label,
-                let geneTypeArray = parameters[label] as? Array<Dictionary<String, Int>> else {
-                    return .failure(EvolverError.encodeError)
-            }
-            let geneSize = GeneCodingKeys.geneSize.stringValue
-            let value = GeneCodingKeys.value.stringValue
-
-            for dictionary in geneTypeArray {
-                array.append(
-                    [
-                        geneSize: dictionary[geneSize] ?? 0,
-                        value: 0
-                    ]
-                )
-            }
-            dictionary[label] = array
-        }
-
-        var genoms: [Genom<T>] = []
-
-        // MARK: Create initial individuals
-        for _ in 0..<individuals {
-            var model = Genom<T>(gene: dictionary)
-            model.randomize()
-            genoms.append(model)
-        }
-
+        // MARK: Unnecessary Evaluate if genom has only one gene
         if individuals == 1 {
             guard let gene = genoms[0].decode() else {
                 return .failure(EvolverError.decodeError)
@@ -106,6 +71,54 @@ public class Evolver {
         }
 
         return .success(gene)
+    }
+
+    private class func checkTemplate<T: Generable>(template: T.Type) throws -> [String: Array<[String : Int]>] {
+        let geneTemplate = template.init()
+
+        let encoder = JSONEncoder()
+        let data = try! encoder.encode(geneTemplate)
+        guard let parameters = ((try? JSONSerialization.jsonObject(with: data, options: .allowFragments)).flatMap { $0 as? [String: Any] }) else {
+            throw EvolverError.encodeError
+        }
+
+        var dictionary = [String: Array<[String : Int]>]()
+        for child in Mirror(reflecting: geneTemplate).children {
+            var array = [Dictionary<String, Int>]()
+
+            guard
+                let label = child.label,
+                let geneTypeArray = parameters[label] as? Array<Dictionary<String, Int>> else {
+                    throw EvolverError.encodeError
+            }
+
+            let geneSize = GeneCodingKeys.geneSize.stringValue
+            let value = GeneCodingKeys.value.stringValue
+
+            for dictionary in geneTypeArray {
+                array.append(
+                    [
+                        geneSize: dictionary[geneSize] ?? 0,
+                        value: 0
+                    ]
+                )
+            }
+            dictionary[label] = array
+        }
+
+        return dictionary
+    }
+
+    private class func createInitialIndividuals<T: Generable>(_ : T.Type, dictionary: [String: Array<[String : Int]>], individuals: Int) -> [Genom<T>] {
+        var genoms: [Genom<T>] = []
+
+        for _ in 0 ..< individuals {
+            var model = Genom<T>(gene: dictionary)
+            model.randomize()
+            genoms.append(model)
+        }
+
+        return genoms
     }
 
     enum EvolverError: Error {
